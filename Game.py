cc4bea106ff5bf947b59008865b54a2aeaebddd6
Антон Ильин <anton.ilin080599@yandex.ru>
2019-10-10 18:08:26 +0300

    hw_2

--- Game.py
+++ Game.py
@@ -3,38 +3,40 @@ class Game:
         self.field = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
     def is_win(self, symbol):
+        '''Victory Check'''
         main_diag = 0
         sup_diag = 0
-        line = [0, 0 ,0]
+        line = [0, 0, 0]
         collum = [0, 0, 0]
-        for i in range(len(self.field)):
-            if self.field[i] == symbol:
-                line[i % 3] += 1
-                collum[int(i / 3)] += 1
-                if (i % 3) == int(i / 3):
+        for index in range(len(self.field)):
+            if self.field[index] == symbol:
+                line[index % 3] += 1
+                collum[int(index / 3)] += 1
+                if (index % 3) == int(index / 3):
                     main_diag += 1
-                if ((i % 3) + int(i / 3)) == 2:
+                if ((index % 3) + int(index / 3)) == 2:
                     sup_diag += 1
         if main_diag == 3 or sup_diag == 3:
             return True
-        for i in line:
-            if i == 3:
+        for index in line:
+            if index == 3:
                 return True
-        for i in collum:
-            if i == 3:
+        for index in collum:
+            if index == 3:
                 return True
         return False
 
     def start_game(self):
-        for i in range(len(self.field)):
-            if i % 3 == 0 and i != 0:
+        '''Process of game'''
+        for index in range(len(self.field)):
+            if index % 3 == 0 and index != 0:
                 print()
-            print(i, end=' ')
+            print(index, end=' ')
         print()
         used_square = set()
         pos_numbers = set()
-        for i in range(len(self.field)):
-            pos_numbers.add(str(i))
+        for index in range(len(self.field)):
+            pos_numbers.add(str(index))
         player = 1
         while len(used_square) < len(self.field):
             print("Введите позицию:")
@@ -42,37 +44,38 @@ class Game:
             if position in used_square:
                 print("Поле занято")
                 continue
-            elif not (position in pos_numbers):
-                print("Некорретный ввод")
-                continue
-            else:
+            if position in pos_numbers:
                 used_square.add(position)
                 if player % 2 == 1:
                     self.field[int(position)] = 'X'
                     if self.is_win('X'):
                         print("Выиграли : X")
-                        game.show_field()
+                        self.show_field()
                         return 1
                     player += 1
                 else:
                     self.field[int(position)] = 'O'
                     if self.is_win('O'):
                         print("Выиграли : O")
-                        game.show_field()
+                        self.show_field()
                         return 2
                     player += 1
-            game.show_field()
+            else:
+                print("Некорретный ввод")
+                continue
+            self.show_field()
         print("Игра окончена")
         return 3
 
     def show_field(self):
-        for i in range(len(self.field)):
-            if i % 3 == 0 and i != 0:
+        '''Shows current field'''
+        for index in range(len(self.field)):
+            if index % 3 == 0 and index != 0:
                 print()
-            print(self.field[i], end=' | ')
+            print(self.field[index], end=' | ')
         print()
 
 
 if __name__ == '__main__':
-    game = Game()
-    game.start_game()
+    our_game = Game()
+    our_game.start_game()
--- Tests.py
+++ Tests.py
@@ -1,25 +1,29 @@
 import unittest
-import Game
+import game
 
 
 class TestGame(unittest.TestCase):
+    '''Testing of game'''
     def test_win1(self):
-        game = Game.Game()
-        game.field = ["X", " ", "X", "O", "X", "X", "O", "X", "X"]
-        self.assertEqual(True, game.is_win("X"))
-        self.assertEqual(False, game.is_win("O"))
+        '''Testing on win'''
+        our_game = game.Game()
+        our_game.field = ["X", " ", "X", "O", "X", "X", "O", "X", "X"]
+        self.assertEqual(True, our_game.is_win("X"))
+        self.assertEqual(False, our_game.is_win("O"))
 
     def test_win2(self):
-        game = Game.Game()
-        game.field = ["X", " ", "O", "O", "O", "X", "O", "X", "X"]
-        self.assertEqual(True, game.is_win("O"))
-        self.assertEqual(False, game.is_win("X"))
+        '''Testing on win'''
+        our_game = game.Game()
+        our_game.field = ["X", " ", "O", "O", "O", "X", "O", "X", "X"]
+        self.assertEqual(True, our_game.is_win("O"))
+        self.assertEqual(False, our_game.is_win("X"))
 
     def test_wrong(self):
-        game = Game.Game()
-        game.field = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        self.assertEqual(False, game.is_win("O"))
-        self.assertEqual(False, game.is_win("X"))
+        '''Testing on loose'''
+        our_game = game.Game()
+        our_game.field = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+        self.assertEqual(False, our_game.is_win("O"))
+        self.assertEqual(False, our_game.is_win("X"))
 
 
 if __name__ == '__main__':
