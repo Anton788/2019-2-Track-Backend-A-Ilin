a08bc05956f048ad1f2fc1d20805e38e85481989
Антон Ильин <anton.ilin080599@yandex.ru>
2019-10-10 17:46:14 +0300

    hw 2

--- Tests.py
+++ Tests.py
@@ -3,11 +3,17 @@ import Game
 
 
 class TestGame(unittest.TestCase):
-    def test_win(self):
+    def test_win1(self):
         game = Game.Game()
         game.field = ["X", " ", "X", "O", "X", "X", "O", "X", "X"]
         self.assertEqual(True, game.is_win("X"))
         self.assertEqual(False, game.is_win("O"))
+
+    def test_win2(self):
+        game = Game.Game()
+        game.field = ["X", " ", "O", "O", "O", "X", "O", "X", "X"]
+        self.assertEqual(True, game.is_win("O"))
+        self.assertEqual(False, game.is_win("X"))
 
     def test_wrong(self):
         game = Game.Game()
