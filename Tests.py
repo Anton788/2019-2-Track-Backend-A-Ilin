9e8e72b91a8a9bde7d87a2f336d017e187d10c37
Антон Ильин <anton.ilin080599@yandex.ru>
2019-10-10 18:18:47 +0300

    _hw_2

--- Tests.py
+++ Tests.py
@@ -8,22 +8,22 @@ class TestGame(unittest.TestCase):
         '''Testing on win'''
         our_game = game.Game()
         our_game.field = ["X", " ", "X", "O", "X", "X", "O", "X", "X"]
-        self.assertEqual(True, our_game.is_win("X"))
-        self.assertEqual(False, our_game.is_win("O"))
+        self.assertTrue(our_game.is_win("X"))
+        self.assertFalse(our_game.is_win("O"))
 
     def test_win2(self):
         '''Testing on win'''
         our_game = game.Game()
         our_game.field = ["X", " ", "O", "O", "O", "X", "O", "X", "X"]
-        self.assertEqual(True, our_game.is_win("O"))
-        self.assertEqual(False, our_game.is_win("X"))
+        self.assertTrue(our_game.is_win("O"))
+        self.assertFalse(our_game.is_win("X"))
 
     def test_wrong(self):
         '''Testing on loose'''
         our_game = game.Game()
         our_game.field = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-        self.assertEqual(False, our_game.is_win("O"))
-        self.assertEqual(False, our_game.is_win("X"))
+        self.assertFalse(our_game.is_win("O"))
+        self.assertFalse(our_game.is_win("X"))
 
 
 if __name__ == '__main__':
